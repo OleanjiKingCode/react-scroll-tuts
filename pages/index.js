19a197c89237aa772baf24f720134afbbb2c072e
OleanjiKingCode <holamilean56@gmail.com>
2022-11-28 00:38:56 +0100

    react-scroll

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import * as Scroll from "react-scroll";
+import { Link, Element } from "react-scroll";
 
 export default function Home() {
   const pagesAndDetails = [
-    { title: "PAGE ONE", details: "Details of Page One", color: "#36897A" },
-    { title: "PAGE TWO", details: "Details of Page Two", color: "#E9E948" },
-    { title: "PAGE THREE", details: "Details of Page Three", color: "#8D1DC5" },
-    { title: "PAGE FOUR", details: "Details of Page Four", color: "#52192B" },
+    { title: "PAGE ONE", details: "Details of Page One" },
+    { title: "PAGE TWO", details: "Details of Page Two" },
+    { title: "PAGE THREE", details: "Details of Page Three" },
+    { title: "PAGE FOUR", details: "Details of Page Four" },
   ];
-  const colors = ["#36897A", "#E9E948", "#8D1DC5", "#52192B"];
+  const colors = ["#b8c7ce ", "#E9E948", "#f1f1f1 ", "#7bc191"];
   return (
     <>
       <Head>
@@ -21,29 +23,58 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-full ">
+      <div className="w-full pt-5 ">
+        <div className="flex flex-wrap items-center justify-between px-12 ">
+          <Link
+            activeClass="active"
+            to="PAGE ONE"
+            spy
+            smooth
+            duration={70}
+            className="px-3 py-3 mb-2 bg-[#b8c7ce] rounded-xl border-2 border-white cursor-pointer"
+          >
+            Go To Page One
+          </Link>
+          <Link
+            activeClass="active"
+            to="PAGE TWO"
+            spy
+            smooth
+            duration={700}
+            className="px-3 py-3 mb-2 bg-[#E9E948] rounded-xl border-2 border-white cursor-pointer"
+          >
+            Go To Page Two
+          </Link>
+          <Link
+            activeClass="active"
+            to="PAGE THREE"
+            spy
+            smooth
+            duration={700}
+            className="px-3 py-3 mb-2 bg-[#f1f1f1] rounded-xl border-2 border-white cursor-pointer"
+          >
+            Go To Page Three
+          </Link>
+          <Link
+            activeClass="active"
+            to="PAGE FOUR"
+            spy
+            smooth
+            duration={700}
+            className="px-3 py-3 mb-2 bg-[#7bc191] rounded-xl border-2 border-white cursor-pointer"
+          >
+            Go To Page Four
+          </Link>
+        </div>
         {pagesAndDetails.map((page, i) => (
           <div key={i}>
-            <div
+            <Element
+              name={page.title}
               className={`w-full h-screen py-3`}
               style={{
                 backgroundColor: colors[i],
               }}
             >
-              <div className="flex flex-wrap items-center justify-between px-12 ">
-                <div className="px-3 py-3 mb-2 bg-[#36897A] rounded-xl text-white">
-                  Go To Page One
-                </div>
-                <div className="px-3 py-3 mb-2 bg-[#E9E948] rounded-xl text-white">
-                  Go To Page Two
-                </div>
-                <div className="px-3 py-3 mb-2 bg-[#8D1DC5] rounded-xl text-white">
-                  Go To Page Three
-                </div>
-                <div className="px-3 py-3 mb-2 bg-[#52192B] rounded-xl text-white">
-                  Go To Page Four
-                </div>
-              </div>
               <div
                 className={`w-full h-full -mt-16 flex flex-col items-center text-center justify-center `}
               >
@@ -52,7 +83,7 @@ export default function Home() {
                 </div>
                 <div className="w-full  text-sm">{page.details}</div>
               </div>
-            </div>
+            </Element>
           </div>
         ))}
       </div>
